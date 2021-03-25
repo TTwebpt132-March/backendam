4e672e129b8eabc90c62f7807dd9ea5ffd673324
Amanda Nelson <amanda.gaylord@gmail.com>
2021-03-24 21:58:22 -0400

    init Node backend with PostgresQL

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
+  },
+
+  testing: {
+    client: "pg",
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: pgConnection,
+      ssl: { rejectedUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+};
