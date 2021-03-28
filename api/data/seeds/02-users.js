2f94230e04aa892d8ac34412dc44c1c819c382d6
Amanda Nelson <amanda.gaylord@gmail.com>
2021-03-28 19:32:37 -0400

    migration and seeds added for both users and recipes

---
+++ api/data/seeds/02-users.js
@@ -0,0 +1,34 @@
+exports.seed = async function (knex) {
+  /*  await knex.schema.createTable("users", (users) => {
+    users.increments("user_id");
+    users.string("user_username", 20).notNullable();
+    users.string("user_password", 25).notNullable();
+    users.string("user_email", 100).notNullable();
+    users.timestamps(false, true); */
+  await knex("users").insert([
+    {
+      user_id: 1,
+      user_username: "MaloryArcher",
+      user_password: "guest",
+      user_email: "malory@isis.com",
+    },
+    {
+      user_id: 2,
+      user_username: "SterlingArcher",
+      user_password: "woodhouse",
+      user_email: "sterling@isis.com",
+    },
+    {
+      user_id: 3,
+      user_username: "LanaKane",
+      user_password: "Abbiejean123",
+      user_email: "lana@isis.com",
+    },
+    {
+      user_id: 4,
+      user_username: "CyrilFiggis",
+      user_password: "elcantador",
+      user_email: "cyril@isis.com",
+    },
+  ]);
+};
