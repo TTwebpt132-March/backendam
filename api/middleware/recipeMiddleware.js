a756167612f98e3ee9e1987dd113ad1ebee21612
Amanda Nelson <amanda.gaylord@gmail.com>
2021-03-29 15:15:56 -0400

    recipes router touched up

--- api/middleware/recipeMiddleware.js
+++ api/middleware/recipeMiddleware.js
@@ -5,6 +5,33 @@ Project Requirement that will need middleware:
 */
 const recipes = require("../models/recipe-model");
 
+const checkRecipeData = (req, res, next) => {
+  const { title, ingredients, instructions, category, user_id } = req.body;
+  if (!Object.keys(req.body).length) {
+    return res.status(409).json({
+      message: "No data was sent to the server",
+    });
+  }
+  if (!title || !ingredients || !instructions || !category || !user_id) {
+    return res.status(409).json({
+      message:
+        "The following are required for the recipe: 'title', 'ingredients', 'instructions', 'category', and 'user_id'. There is also an optional 'recipe_source' that can be added.",
+    });
+  }
+  next();
+};
+const checkRecipeId = async (req, res, next) => {
+  try {
+    const recipe = await recipes.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({
+        message: "There was no recipe found with the ID provided.",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 module.exports = {
   checkRecipeData,
   checkRecipeId,
--- api/routers/recipe-router.js
+++ api/routers/recipe-router.js
@@ -1,3 +1,104 @@
-const { router } = require("../server");
+/*
+Project requirements:
+[GET] all recipes in database - async
+[GET] single recipe by ID - async
+[POST] new recipe - async
+[PUT] edit recipe by ID - async
+[DELETE] a single recipe by ID - async
+Middleware created: checkRecipeData, checkRecipeId
+*/
+const express = require("express");
+const recipes = require("../models/recipe-model");
+const {
+  checkRecipeData,
+  checkRecipeId,
+} = require("../middleware/recipeMiddleware");
+const router = express.Router();
 
-module.exports = router
+//[GET] all recipes in database - async
+router.get("/", async (req, res, next) => {
+  try {
+    const allRecipes = await recipes.find();
+    return res.json(allRecipes);
+  } catch (err) {
+    next(err);
+  }
+});
+//[GET] single recipe by ID - async
+router.get("/:id", checkRecipeId, async (req, res, next) => {
+  try {
+    const singleRecipe = await recipes.findById(req.params.id);
+    return res.json(singleRecipe);
+  } catch (err) {
+    next(err);
+  }
+});
+//[POST] new recipe - async
+router.post("/", checkRecipeData, async (req, res, next) => {
+  try {
+    const {
+      title: recipe_title,
+      source: recipe_source,
+      ingredients: recipe_ingredients,
+      instructions: recipe_instructions,
+      category: recipe_category,
+      photo: recipe_photo_src,
+      user_id: user_id,
+    } = req.body;
+    const newRecipe = await recipes.addRecipe({
+      recipe_title,
+      recipe_source,
+      recipe_ingredients,
+      recipe_instructions,
+      recipe_category,
+      recipe_photo_src,
+      user_id,
+    });
+    return res.json(newRecipe);
+  } catch (err) {
+    next(err);
+  }
+});
+//[PUT] edit recipe by ID - async
+router.put("/:id", checkRecipeId, checkRecipeData, async (req, res, next) => {
+  try {
+    const {
+      title: recipe_title,
+      source: recipe_source,
+      ingredients: recipe_ingredients,
+      instructions: recipe_instructions,
+      category: recipe_category,
+      photo: recipe_photo_src,
+      user_id: user_id,
+    } = req.body;
+    const updatedRecipe = await recipes.updateRecipe(
+      {
+        recipe_title,
+        recipe_source,
+        recipe_ingredients,
+        recipe_instructions,
+        recipe_category,
+        recipe_photo_src,
+        user_id,
+      },
+      req.params.id
+    );
+    return res.json(updatedRecipe);
+  } catch (err) {
+    next(err);
+  }
+});
+//[DELETE] a single recipe by ID - async
+router.delete("/:id", checkRecipeId, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await recipes.deleteRecipe(id);
+    return res.json({
+      message: `The requested recipe has been deleted.`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
